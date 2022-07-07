99ec6ff6d770a1d67c7c3de3863283e3a3d3f2a0
Your Name <xi03hy@gmail.com>
2022-07-07 15:35:35 +0900

    메인 페이지

---
+++ src/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReviewList from './ReviewList';
+import ReviewForm from './ReviewForm';
+
+const Main = () => {
+    return (
+        <div>
+           <ReviewList></ReviewList>
+           <ReviewForm></ReviewForm>
+        </div>
+    );
+};
+
+export default Main;
