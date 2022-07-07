c2fe38dd93519befd4905e82915a22f16187d8f5
Your Name <xi03hy@gmail.com>
2022-07-07 15:43:46 +0900

    리뷰 폼

---
+++ src/ReviewForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const ReviewForm = () => {
+    const [title, setTitle] = useState('');
+    const [rating, setRating] = useState(0)
+    const [content, setContent] = useState('');
+    
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleRatingChange = (e) => {
+        const nextRating = Number(e.target.value);
+        setRating(nextRating);
+    };
+
+    const handleContentChange = (e) => {
+        setContent(e.target.value);
+    };
+
+    return (
+        <form>
+            <input value={title} onChange={ handleTitleChange}></input>
+            <input type='number' value={rating} onChange={handleRatingChange} />
+            <textarea value={content} onChange={handleContentChange} />
+        </form>
+    );
+};
+
+export default ReviewForm;
